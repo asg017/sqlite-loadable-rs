c0d436c154dd39251d3197f80f455bb1d613b17e
Jasm Sison <jasm.sison@gmail.com>
2023-10-05 03:08:41 +0200

    add tests in examples for hyperfine

---
+++ benchmarks/vfs/io_uring/examples/test_5.rs
@@ -0,0 +1,31 @@
+use std::env;
+use rand::Rng;
+use rand::thread_rng;
+
+include!("conn.in");
+
+fn main() -> rusqlite::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let mut conn = create_test_database(args.len() > 0);
+    let mut rng = rand::thread_rng();
+
+    let tx = conn.transaction()?;
+    for _ in 0..25000 {
+        let value1: i32 = rng.gen();
+        let value2: i32 = rng.gen();
+        let value3: String = format!("Value{}", thread_rng().gen_range(0..25000));
+
+        tx.execute("INSERT INTO t5 (a, b, c) VALUES (?, ?, ?)",
+                            (value1, value2, value3))?;
+    }
+    tx.commit()?;
+    
+    let tx2 = conn.transaction()?;
+    for i in 0..9 {
+        let stmt = format!("SELECT count(*), avg(b) FROM t5 WHERE c LIKE '%{}%'", i).to_string();
+        let _ = tx2.execute(&stmt, ());
+    }
+    tx2.commit()?;
+    Ok(())
+}
