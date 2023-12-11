2e089c36a84484ac4e16472a09fb710046aa5a13
Jasm Sison <jasm.sison@gmail.com>
2023-12-11 17:50:02 +0100

    adapted sql script to test the loadable vfs

--- benchmarks/vfs/io_uring/iouring.sql
+++ benchmarks/vfs/io_uring/iouring.sql
@@ -1,19 +1,28 @@
+-- HOWTO
+-- 0. Make sure the sqlite3 was built with the build tag: SQLITE3VFS_LOADABLE_EXT
+-- 1. In Cargo.toml, disable the 'static' feature, from the sqlite-loadable library
+--   it should look like this: sqlite-loadable = {path="../../../"}
+-- 2. Cargo build
+-- 3. load this script: sqlite3 --init iouring.sql
+
+-- This script was tested on 3.44.2, compiled with:
+--   gcc -g -DSQLITE_DEBUG shell.c sqlite3.c -lpthread -ldl -o sqlite3
+
 .mode box
 .header on
 
 .load target/debug/lib_iouringvfs
-.eqp full
--- trace not supported in version: 3.40.1 2022-12-28 14:03:47 df5c253c0b3dd24916e4ec7cf77d3db5294cc9fd45ae7b9c5e82ad8197f3alt1
 
-SELECT io_uring_vfs_from_file('iouring-ext.db');
+--ATTACH io_uring_vfs_from_file('iouring.db') AS "iouring0";
+--SELECT io_uring_vfs_from_file('iouring.db');
 
-ATTACH io_uring_vfs_from_file('iouring-ext.db') AS "iouring-ext";
+ATTACH 'file:iouring.db?vfs=iouring' as iouring;
 
-.open "iouring-ext.db"
+.open "iouring.db"
 
 .vfslist
 
-CREATE TABLE t3(x varchar(10), y integer);
+CREATE TABLE IF NOT EXISTS t3(x varchar(10), y integer);
 
 INSERT INTO t3 VALUES('a', 4),
                      ('b', 5),
